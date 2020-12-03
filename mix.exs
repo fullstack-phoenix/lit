0292fa0f9e25a7701506388c87ad6d265e9686a1
Andreas Eriksson <andreas@codered.se>
2020-12-03 21:46:18 +0100

    Bump to version 0.1.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Lit.MixProject do
   def project do
     [
       app: :lit,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
